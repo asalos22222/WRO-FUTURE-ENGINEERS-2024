30ac27403762bee3b1a08beb0c046fa92730dc8c
asalos22222 <159884853+asalos22222@users.noreply.github.com>
2024-09-26 17:53:56 +0200

    Update Code Arduino.ino

--- src/Code Arduino.ino
+++ src/Code Arduino.ino
@@ -1,88 +1,118 @@
 #include <Servo.h>
-#include <movingAvg.h>
 
-const int servoPin = 9;
+const int TRIGGER_PIN_FRONT = 30;  // Front sonar sensor trigger pin
+const int ECHO_PIN_FRONT = 31;     // Front sonar sensor echo pin
 
-int _output_PD5 = 43;
-int _output_PD4 = 45;
-int _output_PD7 = 41;
-int _output_PD6 = 39;
-int _output_PWM10 = 11;
-int _output_PWM9 = 10;
+const int TRIGGER_PIN_LEFT = 32;  // Left sonar sensor trigger pin
+const int ECHO_PIN_LEFT = 33;     // Left sonar sensor echo pin
 
-const int trigPin = 12;
-const int echoPin = 13;
+const int TRIGGER_PIN_RIGHT = 34;  // Right sonar sensor trigger pin
+const int ECHO_PIN_RIGHT = 35;     // Right sonar sensor echo pin
 
-int intunecat = 610;
-int luminos = 510;
+const int motorPWM = 5;  // PWM pin for the motor
 
-bool isturning = 0;
-unsigned long turnmillis = 0;
+Servo steeringServo;
 
-Servo myservo;
-movingAvg mySensor(10);
+const int forwardSpeed = 255;  // Adjust this value for the desired forward speed
+const int turnSpeed = 255;     // Adjust this value for the desired turn speed
+
+
+// Stop the car if pulse count is greater than 34000
+int lapCount = 1;
+const int totalLaps = 3;  // Set the total number of laps
 
 void setup() {
-    pinMode(_output_PD5, OUTPUT);
-    pinMode(_output_PD4, OUTPUT);
-    pinMode(_output_PD7, OUTPUT);
-    pinMode(_output_PD6, OUTPUT);
-    pinMode(_output_PWM10, OUTPUT);
-    pinMode(_output_PWM9, OUTPUT);
-    pinMode(A0, INPUT);
+  Serial.begin(9600);
+  steeringServo.attach(9);
 
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
+  // Motor
+  pinMode(motorPWM, OUTPUT);
 
-    myservo.attach(servoPin);
-    delay(100);
-    myservo.write(90);
-    delay(100);
-    mySensor.begin();
+  // Sonar Sensors
+  pinMode(TRIGGER_PIN_FRONT, OUTPUT);
+  pinMode(ECHO_PIN_FRONT, INPUT);
+  pinMode(TRIGGER_PIN_LEFT, OUTPUT);
+  pinMode(ECHO_PIN_LEFT, INPUT);
+  pinMode(TRIGGER_PIN_RIGHT, OUTPUT);
+  pinMode(ECHO_PIN_RIGHT, INPUT);
 
-    Serial.begin(9600);
-}
 
-void inainte() {
-    myservo.write(90);
-    digitalWrite(_output_PD4, LOW);
-    digitalWrite(_output_PD5, HIGH);
-    digitalWrite(_output_PD6, LOW);
-    digitalWrite(_output_PD7, HIGH);
-    analogWrite(_output_PWM9, 100);
-    analogWrite(_output_PWM10, 100);
-}
-
-void turnLeft() {
-    myservo.write(45);
-    digitalWrite(_output_PD4, LOW);
-    digitalWrite(_output_PD5, HIGH);
-    digitalWrite(_output_PD6, LOW);
-    digitalWrite(_output_PD7, HIGH);
-    analogWrite(_output_PWM9, 100);
-    analogWrite(_output_PWM10, 100);
 }
 
 void loop() {
-    unsigned long currentmillis = millis();
-    int sensorData = analogRead(A0);
-    int sensorMovingAvg = mySensor.reading(sensorData);
+  int distanceFront = getDistance(TRIGGER_PIN_FRONT, ECHO_PIN_FRONT);
+  int distanceLeft = getDistance(TRIGGER_PIN_LEFT, ECHO_PIN_LEFT);
+  int distanceRight = getDistance(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT);
 
-    Serial.println(sensorMovingAvg);
+  // Print sensor readings
+  Serial.print("Front Distance: ");
+  Serial.print(distanceFront);
+  Serial.print(" cm, Left Distance: ");
+  Serial.print(distanceLeft);
+  Serial.print(" cm, Right Distance: ");
+  Serial.print(distanceRight);
+  Serial.print(" cm, Current position: ");
+ 
 
-    if (sensorMovingAvg > 590) {
-        isturning = 1;
-        turnmillis = currentmillis;
+  // Implement obstacle avoidance based on sensor readings
+  if (distanceFront >= 0 && distanceFront <= 50) {
+    // Obstacle avoidance logic
+    if (distanceRight > distanceLeft) {
+      steeringServo.write(50);
+      moveForward(turnSpeed);
+      delay(200);
+    } else if (distanceLeft > distanceRight) {
+      steeringServo.write(140);
+      moveForward(turnSpeed);
+      delay(200);
     }
-
-    if (currentmillis - turnmillis > 1950) {
-        isturning = 0;
+  } 
+  // Obstacle avoidance logic when distanceFront is greater than or equal to 40
+  if (distanceFront >= 0 && distanceFront >= 30) {
+    if (distanceRight > distanceLeft) {
+      steeringServo.write(80);
+      moveForward(turnSpeed);
+      delay(20);
+    } else if (distanceLeft > distanceRight) {
+      steeringServo.write(110);
+      moveForward(turnSpeed);
+      delay(20);
     }
+  }
 
-    if (isturning) {
-        turnLeft();
-        mySensor.reset();
-    } else {
-        inainte();
-    }
+  // Critical turn
+  if (distanceRight < 25) {
+    steeringServo.write(110);
+    moveForward(turnSpeed);
+    delay(10);
+  } else if (distanceLeft < 25) {
+    steeringServo.write(80);
+    moveForward(turnSpeed);
+    delay(10);
+  }
+}
+
+int getDistance(int triggerPin, int echoPin) {
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(15);
+  digitalWrite(triggerPin, LOW);
+
+  unsigned long duration = pulseIn(echoPin, HIGH);
+  int distance = duration * 0.0343 / 2;
+  return distance;
+}
+
+void moveForward(int speed) {
+  digitalWrite(motorPWM, HIGH);
+  digitalWrite(26,HIGH);
+  digitalWrite(28,LOW);
+
+}
+
+void moveBack(int speed) {
+  digitalWrite(motorPWM, HIGH);
+  digitalWrite(26,LOW);
+  digitalWrite(28,HIGH);
 }
